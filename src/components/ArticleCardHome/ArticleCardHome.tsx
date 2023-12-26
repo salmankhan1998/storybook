06384a1493668550d7d6cfc39dbe9919b86f5c0e
salmankhan1998 <skhandilshad1998@gamil.com>
2023-12-26 13:56:55 -0500

    fixed made to article card

--- src/components/ArticleCardHome/ArticleCardHome.tsx
+++ src/components/ArticleCardHome/ArticleCardHome.tsx
@@ -29,14 +29,18 @@ export default function ArticleCard({
   onClick,
 }: ArticleProps) {
   return (
-    <div data-testid="ArticleCard" className="flex flex-col mt-14 gap-y-4">
+    <div data-testid="ArticleCard" className="flex flex-col gap-y-4 shadow-lg rounded-md p-4 w-full max-w-4xl">
       <div className="flex flex-row gap-x-14">
         <div className="relative">
           <div className="absolute top-6 left-8">
             <Typo variant="H5" title={`0${id + 1}.`} className="text-[48px]" />
           </div>
-          <div className="w-[410px] h-[330px] flex-shrink-0">
-            <Image src={articleImage ? articleImage : ''} alt="article image" width={200} height={200} />
+          <div className="w-[370px] rounded-lg overflow-hidden">
+            <Image src={articleImage ? articleImage : ''} alt="article image"
+              width={370}
+              height={330}
+              objectFit="cover"
+            />
           </div>
           <div className="absolute bottom-6 left-8">
             <Pill title="Bookworm Valley" variant="yellow" />
